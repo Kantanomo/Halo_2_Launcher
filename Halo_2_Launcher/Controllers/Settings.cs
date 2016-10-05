f3e06ee6df88063ba6a360d03d199badce6ce897
Kantanomo <mhatto00@gmail.com>
2016-10-04 22:34:37 -0500

    Revert "Revert "No Hud Hotkey Added""
    This reverts commit 7e4b8b513e9b02560ecbc88abf9c7084524b2cac.

--- Halo_2_Launcher/Controllers/Settings.cs
+++ Halo_2_Launcher/Controllers/Settings.cs
@@ -26,6 +26,7 @@ namespace Halo_2_Launcher.Controllers
         private string _RememberUsername = "";
         private string _LauncherStyle = "Orange";
         private string _xDelayHotkey = "";
+        private string _noHUDHotkey = "";
         public int ResolutionWidth
         {
             get { return this._ResolutionWidth; }
@@ -90,6 +91,11 @@ namespace Halo_2_Launcher.Controllers
         {
             get { return this._xDelayHotkey; }
             set { this._xDelayHotkey = value; }
+        }
+        public string noHUDHotkey
+        {
+            get { return this._noHUDHotkey; }
+            set { this._noHUDHotkey = value; }
         }
         private int GetPrimaryMonitor()
         {
@@ -177,6 +183,11 @@ namespace Halo_2_Launcher.Controllers
                                 this._xDelayHotkey = Setting[1];
                                 break;
                             }
+                        case "noHudHotkey":
+                            {
+                                this._noHUDHotkey = Setting[1];
+                                break;
+                            }
                     }
                 }
             }
@@ -198,6 +209,7 @@ namespace Halo_2_Launcher.Controllers
             SB.AppendLine("RememberUsername:" + RememberUsername);
             SB.AppendLine("LauncherStyle:" + LauncherStyle);
             SB.AppendLine("xDelayHotkey:" + xDelayHotkey);
+            SB.AppendLine("noHudHotkey:" + noHUDHotkey);
             SW.Write(SB.ToString());
             SW.Flush();
             SW.Close();
